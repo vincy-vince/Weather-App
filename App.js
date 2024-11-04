4c97351adfdb466906527e83895750342bd798d3
vincy <vincyvince510@gmail.com>
2024-11-04 11:45:57 +0530

    saves some images and upgraded the UI

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temp = document.querySelector('.temperature-degree')
+  let temp = document.querySelector(".temperature-degree");
   let tempInner = temp.innerHTML;
-console.log(tempInner);
-if(tempInner >= 30){
- const bodyEle = document.querySelector('.js-body');
- console.log(bodyEle);
- 
- bodyEle.classList.add('body-hot');
-}else if (tempInner <= 10){
-  const bodyEle = document.querySelector('.js-body');
-  console.log(bodyEle);
 
-  bodyEle.classList.add('body-cold');
-} else if(tempInner == 24){
-  const bodyEle = document.querySelector('.js-body');
-  console.log(bodyEle);
+  if (tempInner >= 30) {
+    const bodyEle = document.querySelector(".js-body");
 
-  bodyEle.classList.add('body-green');
-}
-else{
-  const bodyEle = document.querySelector('.js-body');
-  bodyEle.classList.remove('body-url')
-}
+    bodyEle.classList.add("body-hot");
+  } else if (tempInner <= 10) {
+    const bodyEle = document.querySelector(".js-body");
 
-//if(lat === ){
-//use dom to get the lat and long from html. 
-//}
+    const express = document.querySelector(".js-temperature-description");
+    express.innerHTML = "its too cold - freezing!";
+
+    bodyEle.classList.add("body-cold");
+  } else if (tempInner == 24) {
+    const bodyEle = document.querySelector(".js-body");
+
+    bodyEle.classList.add("body-green");
+
+    const express = document.querySelector(".js-temperature-description");
+    express.innerHTML = "Perfect weather!";
+  } else if (tempInner == 23) {
+    const bodyEle = document.querySelector(".js-body");
+
+    bodyEle.classList.add("body-rain");
+
+    const express = document.querySelector(".js-temperature-description");
+    express.innerHTML = "Unwrap your Umbrella!";
+  } else {
+    const bodyEle = document.querySelector(".js-body");
+    bodyEle.classList.remove("body-url");
+
+    const express = document.querySelector(".js-temperature-description");
+    express.innerHTML = "Cool!";
+  }
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -35,13 +43,13 @@ else{
       lat = position.coords.latitude;
 
       const api = `https://api.tomorrow.io/v4/weather/realtime?location=${lat},${long}&units=metric&apikey=8WElWxRqhNuuFwfSeSMTPraPSVmYS2FZ`;
-      fetch(api).then((res) => {
-        return res.json()
-      }).then(data => {
-        console.log(data);
-        const temperature = data.data.values.temperature;  
-        console.log(temperature); 
-      });
+      fetch(api)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const temperature = data.data.values.temperature;
+        });
     });
   }
 });
